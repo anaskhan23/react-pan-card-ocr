3daee196ac604bf875b0d63c029ab1e01810ce98
Anas Khan <anaskhan23797@gmail.com>
2020-10-02 12:37:59 +0530

    Updates

---
+++ precache-manifest.abad5a8da8357ae203f14d2218d917d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d708e21023dff7d0971576c9e8a3fe8a",
+    "url": "/react-pan-card-ocr/index.html"
+  },
+  {
+    "revision": "b2652fc36c59c35b63f2",
+    "url": "/react-pan-card-ocr/static/css/main.cdf92dec.chunk.css"
+  },
+  {
+    "revision": "955449aa216f38d707c2",
+    "url": "/react-pan-card-ocr/static/js/2.da19fdab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-pan-card-ocr/static/js/2.da19fdab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2652fc36c59c35b63f2",
+    "url": "/react-pan-card-ocr/static/js/main.e6120f51.chunk.js"
+  },
+  {
+    "revision": "6d9aff7c89fa4e3411b2",
+    "url": "/react-pan-card-ocr/static/js/runtime-main.415f0b28.js"
+  },
+  {
+    "revision": "083af689612909bbd3f3ae971c475f79",
+    "url": "/react-pan-card-ocr/static/media/pan.083af689.jpg"
+  }
+]);
